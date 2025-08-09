06550564ae9ee88e76cab1f2c9769cf2771a4654
refilwe-m <refilwe.dev@gmail.com>
2025-08-09 16:32:24 +0200

    Modularised Twttr Solution

--- Set2/twttr/twttr.py
+++ Set2/twttr/twttr.py
@@ -1,6 +1,15 @@
-text = input('Input: ')
-output = ""
-for char in text:
-    if char not in 'aeiouAEIOU':
-        output+=char
-print('Output: ',output)
+def main():
+   text = input('Input: ')
+   shorten(text)
+
+
+def shorten(word):
+    output = ""
+    for char in word:
+        if char not in 'aeiouAEIOU':
+            output+=char
+    print('Output:',output)
+
+
+if __name__ == "__main__":
+    main()
